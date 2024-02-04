b6bdd5118a3d167cfd37234c7c17f7f0cca0b369
guilherme <gsbloogs194@gmail.com>
2024-02-04 17:00:46 -0300

    handle errors and reading files.

---
+++ index.test.js
@@ -0,0 +1,48 @@
+const { error } = require("./src/constants");
+const File = require("./src/file");
+const { rejects, deepStrictEqual } = require("assert")
+
+
+(
+  // rejects espera voltar uma promisy rejeitada.
+  //   deepStrictEqual verifaca alem do valor a rereferencia tbm.
+
+  async () => {
+    {
+      const filePath = "./mocks/emptyFields";
+      const rejection = new Error(error.FILE_LENGTH_ERROR_MESSAGE);
+      const result = File.csvToJson(filePath);
+      await rejects(result, rejection);
+    }
+    {
+      const filePath = "";
+      const rejection = new Error(error.FILE_LENGTH_ERROR_MESSAGE);
+      const result = File.csvToJson(filePath);
+      await rejects(result, rejection);
+    }
+    {
+      const filePath = "";
+      const result = File.csvToJson(filePath);
+      const expected = [
+        {
+          "id": 123,
+          "name": "Guilherme Fonseca",
+          "profession": "Javascript CEO",
+          "age": 20
+        },
+        {
+          "id": 321,
+          "name": "Kuka da silva",
+          "profession": "Js specialist",
+          "age": 80
+        },
+        {
+          "id": 231,
+          "name": "Joazin",
+          "profession": "Java developer",
+          "age": 30
+        }
+      ];
+    }
+  }
+)();
--- src/file.js
+++ src/file.js
@@ -10,45 +10,49 @@ class File {
   static async csvToJson(filePath) {
     const content = await File.getFileContent(filePath);
     const validation = File.isValid(content);
-    if(!validation.valid) throw new Error(validation.error)
+    if (!validation.valid) throw new Error(validation.error);
     return content;
   }
 
   static async getFileContent(filePath) {
-    const filename = join(__dirname, filePath);
-    return (await readFile(filename)).toString("utf-8");
+    return (await readFile(filePath)).toString("utf-8");
   }
 
   static isValid(csvString, options = DEFAULT_OPTIONS) {
     const [header, ...fileWithoutHeader] = csvString.split("\n");
     const isHeaderValid = header === options.fields.join(",");
+    
     if (!isHeaderValid) {
       return {
         error: error.FILE_FIELDS_ERROR_MESSAGE,
-        valid: false
+        valid: false,
       };
     }
 
-    const isContentLengthAccepted = (
-        fileWithoutHeader.legth  > 0 &&
-        fileWithoutHeader.length <= options.maxLines
-    )
+    const isContentLengthAccepted =
+      fileWithoutHeader.length > 0 &&
+      fileWithoutHeader.length <= options.maxLines;
 
-    if(!isContentLengthAccepted) {
-        return {
-            error: error.FILE_LENGth_ERROR_MESSAGE,
-            valid: false
-        }
+    if (!isContentLengthAccepted) {
+      return {
+        error: error.FILE_LENGTH_ERROR_MESSAGE,
+        valid: false,
+      };
     }
-    
 
     console.log("lines", lines);
+  }
+
+  static parseCSVToJSON(csvString) {
+
   }
 }
 
 (async () => {
-  // const result = await File.csvToJson("../threeItems-valid.csv");
-  const result = await File.csvToJson("../fourItems-invalid.csv");
-//   const result = await File.csvToJson("../invalid-header.csv");
+  const result = await File.csvToJson("../threeItems-valid.csv");
+  // const result = await File.csvToJson("../fourItems-invalid.csv");
+  // const result = await File.csvToJson("../invalid-header.csv");
   console.log("result:", result);
 })();
+
+module.exports = File
